a087b911c57942e9cbbe5b9b5430dcfebe013f27
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 01:19:54 +0530

    Update RWOnly.java

--- main.api/RWOnly.java
+++ main.api/RWOnly.java
@@ -2,6 +2,14 @@
 public class RWOnly {
     private String name;  // Private member variable
     
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     // Constructor (optional)
     public RWOnly(String name) {
         this.name = name;

f7c906d6817fa7d0ab2fcfd29033af4b5998ce9f
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 01:19:59 +0530

    Update Main.java

--- main.api/Main.java
+++ main.api/Main.java
@@ -2,5 +2,11 @@
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly("Initial Name");
+        
+        // Set a new value using setter
+        obj.setName("New Name");
+        
+        // Access the value using getter and print
+        System.out.println(obj.getName());  // Output: New Name
     }
 }

39706b76b835e6fcb4841ac627e0c8c3565d70a0
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-08-27 01:18:20 +0530

    RWOnly.java

---
+++ main.api/RWOnly.java
@@ -0,0 +1,9 @@
+// RWOnly.java
+public class RWOnly {
+    private String name;  // Private member variable
+    
+    // Constructor (optional)
+    public RWOnly(String name) {
+        this.name = name;
+    }
+}
